b79ca16b818eba71ad127d90be87f2eb39a53b1b
Evgeny Berezhnov <jackberezhnov@gmail.com>
2020-03-21 18:21:25 -0700

    add Input component

---
+++ src/components/Input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Input extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Todo List</h1>
+                <input type="text"/>
+            </div>
+        )
+    }
+}
+
+export default Input;
